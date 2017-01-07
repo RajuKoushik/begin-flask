4821396dbb529a5acb25b094c9e72115292f376d
rajukoushik <g.rajukoushik@gmail.com>
2017-01-07 23:52:34 +0530

    commit-jinga

--- begin-flask.py
+++ begin-flask.py
@@ -17,9 +17,9 @@ def login():
         return redirect(url_for('success',name = user))
 
 
-@app.route('/')
-def index():
-   return render_template('hello.html')
+@app.route('/hello/<user>')
+def hello_name(user):
+   return render_template('hello.html', name = user)
 
 if __name__ == '__main__':
     app.run(debug = True)
