edb29d28e88560d3155ac76784116118a91a7a33
rajukoushik <g.rajukoushik@gmail.com>
2017-01-08 11:23:37 +0530

    minor glitches

--- begin-flask.py
+++ begin-flask.py
@@ -19,18 +19,19 @@ def login():
 
 @app.route('/hello/<int:score>')
 def hello_name(score):
-   return render_template('hello.html', marks = score)
+    return render_template('hello.html', marks = score)
 
 
 @app.route('/result')
 def result():
-   dict = {'phy':50,'che':60,'maths':70}
-   return render_template('result.html', result = dict)
+    dict = {'phy':50,'che':60,'maths':70}
+    return render_template('result.html', result = dict)
 
 
 @app.route("/")
 def index():
-   return render_template("index.html")
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug = True)
